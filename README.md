6e6eefc622b7bfbf840c566dfdda89ebda48297f
Chris Wilson <christopher.e.wilson>
2020-12-08 13:19:56 -0500

    Reformatted samples

--- README.md
+++ README.md
@@ -8,10 +8,14 @@ Every possible scale in intervals of 1, 2 or 3 half-steps
 
 ## Samples
 
-The original samples are from the [University of Iowa Musical Studios](http://theremin.music.uiowa.edu/MISpiano.html). This [StackOverflow](https://video.stackexchange.com/questions/23340/how-to-use-ffmpeg-to-fade-in-out-a-veriable-frame-rate-video-clip-with-unknown-d) has useful advice on `ffmpeg`:
+The original samples are from the [University of Iowa Musical Studios](http://theremin.music.uiowa.edu/MISpiano.html). They were downloaded with the following script:
 
-	ffmpeg -i Iowa/Piano.ff.C5.aiff -af loudnorm,silenceremove=start_periods=1:start_silence=0.05:start_threshold=-40dB,afade=out:st=0.75:d=0.25 -to 1 formatted/C5.m4a
+	./samples/download.sh
 
-You can format them all with `./samples/format.sh`
+We want `mp3` files for maximum [browser compatibility](https://blog.filestack.com/thoughts-and-knowledge/audio-file-format-codec/) and file size. The original sample have a small amount of silence in the beginning that needs to be cut, and they need to be reduced to 1 seconds. This [StackOverflow](https://video.stackexchange.com/questions/23340/how-to-use-ffmpeg-to-fade-in-out-a-veriable-frame-rate-video-clip-with-unknown-d) has useful advice on how to do this with `ffmpeg`:
+
+	ffmpeg -i original/Piano.ff.C5.aiff -af loudnorm,silenceremove=start_periods=1:start_silence=0.05:start_threshold=-40dB,afade=out:st=0.75:d=0.25 -to 1 formatted/C5.mp3
+
+You can format them all with `./samples/format.sh`. This will create three samples for each of the 88 notes: a 500ms, 1-second and 2-second version, all `mp3`s.
 
 
--- index.js
+++ index.js
@@ -1,27 +1,64 @@
 import { select, selectAll, event } from 'd3-selection';
 import { transition, duration } from 'd3-transition';
+import Vex from 'vexflow';
 
-const Vex = require('vexflow');
 const VF = Vex.Flow;
 
 const scales = require("./scales.json");
 
+console.log(scales);
+
 require("./styles.scss");
 
-let noteNames = {
-	sharps: ["c/4", "c#/4", "d/4", "d#/4", "e/4", "f/4", "f#/4", "g/4", "g#/4", "a/4", "a#/4", "b/4", "c/5"],
-	flats: ["c/4", "db/4", "d/4", "eb/4", "e/4", "f/4", "gb/4", "g/4", "ab/4", "a/4", "bb/4", "b/4", "c/5"]
-}
+const NOTES = [
+	{ name: "C4",  base: "C", n: "c/4", f: "dbb/4", s: "b#/3" },
+	{ name: "Db4", base: "D", n: null,  f: "db/4",  s: "c#/4"},
+	{ name: "D4",  base: "D", n: "d/4", f: "ebb/4", s: "c##/4"},
+	{ name: "Eb4", base: "E", n: null,  f: "eb/4",  s: "d#/4" },
+	{ name: "E4",  base: "E", n: "e/4", f: "fb/4",  s: "d##/4" },
+	{ name: "F4",  base: "F", n: "f/4", f: "gbb/4", s: "e#/4" },
+	{ name: "Gb4", base: "G", n: null,  f: "gb/4",  s: "f#/4" },
+	{ name: "G4",  base: "G", n: "g/4", f: "abb/4", s: "f##/4" },
+	{ name: "Ab4", base: "A", n: null,  f: "ab/4",  s: "g#/4" },
+	{ name: "A4",  base: "A", n: "a/4", f: "bbb/4", s: "g##/4" },
+	{ name: "Bb4", base: "B", n: null,  f: "bb/4",  s: "a#/4" },
+	{ name: "B4",  base: "B", n: "b/4", f: "cb/4",  s: "a##/4" },
+	{ name: "C5",  base: "C", n: "c/5", f: "dbb/5", s: "b#/4" }
+];
 
-let playNotes = {};
+NOTES.forEach(function(d, i) {
+	let noteName = d.name;
 
-noteNames.flats.forEach(function(d, i) {
-	let noteName = noteNames.flats[i].split("/")[0].toUpperCase() + noteNames.flats[i].split("/")[1];
-	// d.setAttribute("data-note", noteName);
-	var audio = new Audio('./samples/formatted/' + noteName + '.m4a');
-	playNotes[noteName] = audio;
+	d.audio = {
+		500: new Audio('./samples/formatted/500/' + noteName + '.mp3'),
+		1000: new Audio('./samples/formatted/1000/' + noteName + '.mp3'),
+		2000: new Audio('./samples/formatted/2000/' + noteName + '.mp3')
+	};
+
+	// StaveNotes for VF
+	
+	// Natural note (white key) with natural sign
+	d.plain = d.n ? new VF.StaveNote({ clef: "treble", keys: [ d.n ], duration: "4" }) : null;
+	d.natural = d.n ? new VF.StaveNote({ clef: "treble", keys: [ d.n ], duration: "4" }).addAccidental(0, new VF.Accidental("n")) : null;
+
+	// flat
+	let accidental = d.f.split("/")[0].slice(1);
+	d.flat = new VF.StaveNote({ clef: "treble", keys: [ d.f ], duration: "4" }).addAccidental(0, new VF.Accidental(accidental));
+
+	// sharp
+	accidental = d.s.split("/")[0].slice(1);
+	d.sharp = new VF.StaveNote({ clef: "treble", keys: [ d.s ], duration: "4" }).addAccidental(0, new VF.Accidental(accidental));
+
+	// won't include 'bb' since those are all naturals
+	d.canonical = {
+		flats: d.plain ? d.plain : d.flat,
+		sharps: d.plain ? d.plain : d.sharp
+	};
 });
 
+console.log(NOTES)
+
+/*
 let NOTES = {
 	sharps: noteNames.sharps.map(d => {
 		let note = d.split("#");
@@ -38,8 +75,11 @@ let NOTES = {
 		return new VF.StaveNote({clef: "treble", keys: [ d ], duration: "4" }).addAccidental(0, new VF.Accidental("b"));
 	})
 };
+*/
 
 let container = document.querySelector("#scales");
+
+let MODE = "flats";
 
 function drawScale(id) {
 	if (typeof id == "undefined") {
@@ -49,11 +89,15 @@ function drawScale(id) {
 
 	let intervals = scales[id];
 
+	console.log(intervals);
+
 	// https://stackoverflow.com/questions/20477177/creating-an-array-of-cumulative-sum-in-javascript
 	let scale = intervals.reduce(function(r, a) {
 		r.push((r.length && r[r.length - 1] || 0) + a);
 		return r;
 	}, []);
+
+	console.log(scale);
 
 	let time = scale.length + "/4";
 
@@ -61,7 +105,7 @@ function drawScale(id) {
 	div.id = "scale_" + id;
 	container.append(div);
 
-	let renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+	const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
 	renderer.resize(780, 200);
 	let context = renderer.getContext();
 
@@ -70,9 +114,26 @@ function drawScale(id) {
 
 	stave.setContext(context).draw();
 
-	let notes = scale.map(d => {
-		return NOTES.flats[d]
-	});
+
+	// if consecutive notes have the same base and the first is flat, add a natural sign
+
+	let notes = [];
+
+	for (let c = 0; c < scale.length; c += 1) {
+		let n = scale[c];
+		let note;
+		if (c == 0 || MODE !== "flats") {
+			note = NOTES[n].canonical[MODE];
+		} else {
+			if (NOTES[scale[c - 1]].base === NOTES[n].base) {
+				note = NOTES[n].natural;
+			} else {
+				note = NOTES[n].canonical[MODE];
+			}
+		}
+		note.data = NOTES[n];
+		notes.push(note);
+	}
 
 	let voice = new VF.Voice({num_beats: scale.length, beat_value: 4});
 	voice.addTickables(notes);
@@ -85,24 +146,57 @@ function drawScale(id) {
 
 	let nodes = document.querySelectorAll(".vf-stavenote");
 
+	function playNote(index, duration) {
+		let note = notes[index];
+		let node = nodes[index];
+
+		if (duration !== 500 && duration !== 1000 && duration !== 2000) {
+			duration = 1000;
+		}
+
+		let audio = note.data.audio[duration];
+		audio.play();
+		select(node).selectAll("path").style("fill", "red").style("stroke", "red").transition().duration(duration).style("fill", "black").style("stroke", "black");
+	}
+
+	function playScale(tempo, duration) {
+
+		if (typeof tempo === "undefined") {
+			tempo = 1000;
+		}
+
+		if (!duration) {
+			duration = 1000;
+		}		
+
+		for (let c = 0; c < scale.length; c += 1) {
+			setTimeout(function() {
+				playNote(c, duration);
+			}, tempo * c);
+		}
+	}
+
 	nodes.forEach((node, n) => {
-		let i = scale[n];
-		let noteName = noteNames.flats[i].split("/")[0].toUpperCase() + noteNames.flats[i].split("/")[1];
-		node.setAttribute("data-note", noteName);
+		let note = notes[n];
+		node.setAttribute("data-note", note.name);
 
 		node.addEventListener("click", function() {
-			let noteName = this.getAttribute("data-note");
-			console.log(noteName, this);
-			let audio = playNotes[noteName];
-			audio.play();
-
-			let node = select(this);
-			console.log(node);
-			node.selectAll("path").style("fill", "red").style("stroke", "red").transition().duration(1500).style("fill", "black").style("stroke", "black");
-		})
+			playNote(n, 500);
+		});
 
 	});
 
+	return {
+		intervals: intervals,
+		scale: scale,
+		notes: notes,
+		playScale: playScale
+	}
 }
 
-drawScale(301);
+let scale = drawScale(301);
+
+select("#play_scale").on("click", function() {
+	console.log(scale);
+	scale.playScale(500, 500);
+});
